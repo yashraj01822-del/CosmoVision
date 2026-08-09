7fb8177165fa038ddab729d8006faafa00d2bb1c
Astrophotography With Yash <yashraj01822@gmail.com>
2026-08-09 20:54:45 +0530

    Update skyObjects.js

--- src/src/components/src/components/src/components/src/components/src/components/src/services/src/services/src/services/src/services/src/data/skyObjects.js
+++ src/src/components/src/components/src/components/src/components/src/components/src/services/src/services/src/services/src/services/src/data/skyObjects.js
@@ -1,1 +1,73 @@
+export const skyObjects = [
+  {
+    id: "polaris",
+    name: "Polaris",
+    type: "star",
+    ra: 37.9546,
+    dec: 89.2641,
+    magnitude: 1.98
+  },
 
+  {
+    id: "sirius",
+    name: "Sirius",
+    type: "star",
+    ra: 101.2872,
+    dec: -16.7161,
+    magnitude: -1.46
+  },
+
+  {
+    id: "betelgeuse",
+    name: "Betelgeuse",
+    type: "star",
+    ra: 88.7929,
+    dec: 7.4071,
+    magnitude: 0.42
+  },
+
+  {
+    id: "rigel",
+    name: "Rigel",
+    type: "star",
+    ra: 78.6345,
+    dec: -8.2016,
+    magnitude: 0.13
+  },
+
+  {
+    id: "vega",
+    name: "Vega",
+    type: "star",
+    ra: 279.2347,
+    dec: 38.7837,
+    magnitude: 0.03
+  },
+
+  {
+    id: "aldebaran",
+    name: "Aldebaran",
+    type: "star",
+    ra: 68.98,
+    dec: 16.51,
+    magnitude: 0.85
+  },
+
+  {
+    id: "antares",
+    name: "Antares",
+    type: "star",
+    ra: 247.3519,
+    dec: -26.432,
+    magnitude: 1.06
+  },
+
+  {
+    id: "spica",
+    name: "Spica",
+    type: "star",
+    ra: 201.2983,
+    dec: -11.1614,
+    magnitude: 0.98
+  }
+];

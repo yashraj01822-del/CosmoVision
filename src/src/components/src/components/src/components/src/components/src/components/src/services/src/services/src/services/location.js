3761b99d91c63dd2b51f6bdb6f2f3399e0df12fe
Astrophotography With Yash <yashraj01822@gmail.com>
2026-08-09 21:02:15 +0530

    Add getUserLocation function for geolocation

--- src/src/components/src/components/src/components/src/components/src/components/src/services/src/services/src/services/location.js
+++ src/src/components/src/components/src/components/src/components/src/components/src/services/src/services/src/services/location.js
@@ -1,1 +1,32 @@
+export function getUserLocation() {
+  return new Promise((resolve, reject) => {
+    if (!navigator.geolocation) {
+      reject(
+        new Error(
+          "Geolocation is not supported by this browser."
+        )
+      );
+      return;
+    }
 
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        resolve({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          accuracy: position.coords.accuracy
+        });
+      },
+
+      (error) => {
+        reject(error);
+      },
+
+      {
+        enableHighAccuracy: true,
+        timeout: 10000,
+        maximumAge: 300000
+      }
+    );
+  });
+}
